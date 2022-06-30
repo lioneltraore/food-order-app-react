beb902a05f3141ec2043b2dc7292b8f7f20ec3c0
Lionel Traore <hello@lioneltraore.com>
2022-06-30 00:42:40 +0000

    add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Fragment } from 'react';
+import Header from './components/Layout/Header';
 
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
