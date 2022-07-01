fcdb10597509a9254807a71d606cf801f6631951
Lionel Traore <hello@lioneltraore.com>
2022-07-01 23:57:57 +0000

    add meals

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { Fragment } from 'react';
 import Header from './components/Layout/Header';
+import MealsSummary from './components/Meals/MealsSummary';
+import AvailableMeals from './components/Meals/AvailableMeals';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+
+const Meals = () => {
+  return (
+    <section>
+      <MealsSummary />
+      <AvailableMeals />
+    </section>
+  );
+};
+
+export default Meals;
